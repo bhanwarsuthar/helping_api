f29c367f879e4728d8a8ff6973a5a3556fadcf03
Gokul Suthar <gokulsutharofficial@gmail.com>
2023-11-01 16:19:59 +0530

    reject transaction bug fixed

--- migrations/20220214075553-create-transactions.js
+++ migrations/20220214075553-create-transactions.js
@@ -1,52 +1,52 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('ac_ledger_transactions', {
+    await queryInterface.createTable("ac_ledger_transactions", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.BIGINT.UNSIGNED,
-       },
-       user_id: {
-         type: Sequelize.BIGINT.UNSIGNED,
-         references: {
-           model: {
-             tableName: 'users'
-           },
-           key: 'id'
-         },
-         allowNull: false,
-         onDelete: 'CASCADE',
-         onUpdate: 'CASCADE',
-       },
-       ac_ledger_id: {
-         type: Sequelize.BIGINT.UNSIGNED,
-         references: {
-           model: {
-             tableName: 'ac_ledgers'
-           },
-           key: 'id'
-         },
-         allowNull: false,
-         onDelete: 'RESTRICT',
-         onUpdate: 'RESTRICT',
-       },
-       amount: Sequelize.DECIMAL(10,2),
-       tx_type: Sequelize.ENUM(["debit","credit","pending"]),
-       notation: Sequelize.STRING(255),
-       meta: Sequelize.JSON,
+      },
+      user_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        references: {
+          model: {
+            tableName: "users",
+          },
+          key: "id",
+        },
+        allowNull: false,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      ac_ledger_id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        references: {
+          model: {
+            tableName: "ac_ledgers",
+          },
+          key: "id",
+        },
+        allowNull: false,
+        onDelete: "RESTRICT",
+        onUpdate: "RESTRICT",
+      },
+      amount: Sequelize.DECIMAL(10, 2),
+      tx_type: Sequelize.ENUM(["debit", "credit", "pending", "reject"]),
+      notation: Sequelize.STRING(255),
+      meta: Sequelize.JSON,
       created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updated_at: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('ac_ledger_transactions');
-  }
+    await queryInterface.dropTable("ac_ledger_transactions");
+  },
 };
